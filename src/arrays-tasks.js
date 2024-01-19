60e083d08fdc8aa60c6dc4f197c9628fe764d670
[Malykhina Kira] <[kira4ka12@gmail.com]>
2024-01-19 02:02:24 +0200

    feat: add tree functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -38,10 +38,11 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const res1 = arr1.length >= arr2.length ? arr1 : arr2;
+  const res2 = arr1.length < arr2.length ? arr1 : arr2;
+  return res1.map((item, i) => (res2[i] ? item + res2[i] : item));
 }
-
 /**
  * Returns an index of the specified element in array or -1 if element is not found.
  *
@@ -54,10 +55,10 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
-
+// console.log(findElement(['Ace', 10, true], 10));
 /**
  * Returns a number of all occurrences of the specified item in an array.
  *
