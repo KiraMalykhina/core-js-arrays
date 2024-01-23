cadd4ee9adbef2bb94652e6bb2e69dc693060139
[Malykhina Kira] <[kira4ka12@gmail.com]>
2024-01-23 18:35:46 +0200

    add two task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -58,7 +58,7 @@ function sumArrays(arr1, arr2) {
 function findElement(arr, value) {
   return arr.indexOf(value);
 }
-// console.log(findElement(['Ace', 10, true], 10));
+
 /**
  * Returns a number of all occurrences of the specified item in an array.
  *
@@ -73,10 +73,10 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((elem) => elem === item).length;
 }
-
+// console.log(findAllOccurrences([ 'a','b','c','c' ], 'c'));
 /**
  * Removes falsy values from the specified array.
  * Falsy values: false, null, 0, "", undefined, and NaN.
@@ -89,10 +89,10 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((item) => item);
 }
-
+// console.log(removeFalsyValues([ 0, false, 'cat', NaN, true, '' ]));
 /**
  * Returns an array containing the lengths of each string in a specified array of strings.
  *
