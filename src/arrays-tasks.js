9f48c20201f2f6e024a48326af5e20c2d12eb19c
[Malykhina Kira] <[kira4ka12@gmail.com]>
2024-02-08 21:03:15 +0200

    feat: add 1 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -423,10 +423,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(number) {
+  const odds = [];
+  number.map((num, i) => (num % 2 === 1 ? odds.push(i) : null));
+  return odds;
 }
-
+// console.log(getIndicesOfOddNumbers([1, 2, 3, 4, 5]));
 /**
  * Returns the array of RGB Hex strings from the specified array of numbers.
  *
